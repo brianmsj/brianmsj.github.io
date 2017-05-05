6cc042262e4b15ea6db34156c72619ecc343d423
brianmsj <brianmsj@gmail.com>
2017-05-05 09:35:48 -0700

    beta version completed of porfolio

---
+++ app0.js
@@ -0,0 +1,35 @@
+// scrolling function for the page
+$('a[href*="#"]')
+ .not('[href="#"]')
+ .not('[href="#0"]')
+ .click(function(event) {
+   if (
+     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+     &&
+     location.hostname == this.hostname
+   ) {
+     var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+     if (target.length) {
+       event.preventDefault();
+       $('html, body').animate({
+         scrollTop: target.offset().top
+       }, 1000, function() {
+         var $target = $(target);
+         $target.focus();
+         if ($target.is(":focus")) {
+           return false;
+         } else {
+           $target.attr('tabindex','-1');
+           $target.focus();
+         };
+       });
+     }
+   }
+ });
+ document.addEventListener('DOMContentLoaded', function(){
+     Typed.new('.job-title', {
+       strings: ["Web Developer", "Full Stack Web Developer"],
+       typeSpeed: 150
+     });
+ });
